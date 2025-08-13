802f78f9a72c7ac8ad1651cfbb68ba641b119976
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2025-08-13 09:02:42 -0300

    Create Repeticao.py

---
+++ Repeticao.py
@@ -0,0 +1,12 @@
+'''
+for i in range(0,11,1):
+    #print("Numero:",i)
+    print(f"Numero: {i}")
+'''
+from time import sleep
+
+contador = 10
+while contador >=0:
+    print(f"Contador: {contador}")
+    contador -= 1
+    sleep(1)
