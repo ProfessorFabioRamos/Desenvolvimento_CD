34d58fc0b063c347aae1e39b8981036a3b9c9bb1
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2025-08-20 09:06:44 -0300

    Update Exemplos_String.py

--- Exemplos_String.py
+++ Exemplos_String.py
@@ -7,59 +7,85 @@ Estado: DF
 """
 
 # Control + / para comentar diversas linhas
-print(nome)
-print(frase)
-print(texto_muti_linha)
+# print(nome)
+# print(frase)
+# print(texto_muti_linha)
 
 # Fatiamento de string
 texto = "Programação"
-print(texto)        #Programação
-print(texto[0])     #P
-print(texto[1])     #o
-print(texto[0:4])   #Prog
-print(texto[4:])    #ramação
-print(texto[:5])    #Progr
-print(texto[::2])   #Pormço
+# print(texto)        #Programação
+# print(texto[0])     #P
+# print(texto[1])     #o
+# print(texto[0:4])   #Prog
+# print(texto[4:])    #ramação
+# print(texto[:5])    #Progr
+# print(texto[::2])   #Pormço
 
 # Alteração de Caixa
 frase = "Ciência de Dados é o futuro"
-print(frase.lower())
-print(frase.upper())
-print(frase.capitalize())
-print(frase.title())
-print(frase.swapcase())
+# print(frase.lower())
+# print(frase.upper())
+# print(frase.capitalize())
+# print(frase.title())
+# print(frase.swapcase())
 
 #Remoção de espaços
 frase_2 = " Olá, mundo!  "
 
-print(frase_2)
-print(frase_2.strip())  #Remove espaços nas pontas
-print(frase_2.lstrip()) #Remove espaços no início
-print(frase_2.rstrip()) #Remove espaços no final
+# print(frase_2)
+# print(frase_2.strip())  #Remove espaços nas pontas
+# print(frase_2.lstrip()) #Remove espaços no início
+# print(frase_2.rstrip()) #Remove espaços no final
 
 frase_3 = "#Exemplo#"
-print(frase_3.strip("#")) #Remove caractere específico nas pontas
+#print(frase_3.strip("#")) #Remove caractere específico nas pontas
 
 # Substitui conteúdo de substring (pedaço da string)
 mensagem = "Olá Marcos!"
-print(mensagem)
+#print(mensagem)
 nova_mensagem = mensagem.replace("Marcos", "Maria")
-print(nova_mensagem)
+#print(nova_mensagem)
 
 # Verifica se existe o conteúdo dentro da string
 frase_4 = "A IA irá dominar o mundo"
-print("IA" in frase_4)
-print("AI" in frase_4)
-print(frase_4.startswith("A"))      # Verifica se string inicia com valor
-print(frase_4.endswith("mundo"))    # Verifica se string termina com valor
+# print("IA" in frase_4)
+# print("AI" in frase_4)
+# print(frase_4.startswith("A"))      # Verifica se string inicia com valor
+# print(frase_4.endswith("mundo"))    # Verifica se string termina com valor
 
 #Verifica comprimento da string
 palavra = "computador"
-print(len(palavra))
+# print(len(palavra))
+# print(palavra.count("o")) #Quantidade de caracteres correspondentes
+# print(palavra.count("om"))
 
+# Divisão e junção de strings
+frase_5 = "Aprender Python é divertido"
+lista_palavras = frase_5.split()
+#print(lista_palavras)
 
+frase_6 = "-".join(lista_palavras)
+#print(frase_6)
 
+# Encontrar posições
+frase_7 = "Python é muito versátil"
+# print(frase_7.find("é"))
+# print(frase_7.find("Java"))
+# print(frase_7.index("versátil"))
 
+# Formatações
+nome = "Gabriel"
+idade = 25
+altura = 1.8564522
 
+print("Olá, meu nome é "+nome+" e tenho "+str(idade)+" anos.")
+print("Olá, meu nome é",nome,"e tenho",idade,"anos.")
+print("Olá, meu nome é {} e tenho {} anos.".format(nome,idade))
+print("Olá, meu nome é {1} e tenho {0} anos.".format(nome,idade))
+print(f"Olá, meu nome é {nome} e tenho {idade} anos.")
 
-
+import math
+# Formatação de float
+print(f"{nome} tem {altura:.2f}")
+print(math.floor(altura))
+print(math.ceil(altura))
