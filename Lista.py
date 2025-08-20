63775f18cf424b4341d6f3f9ffc487a1f307f26c
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2025-08-20 09:34:37 -0300

    Create Lista.py

---
+++ Lista.py
@@ -0,0 +1,13 @@
+# Listas []
+frutas = ["maça","banana","uva","laranja"]
+
+print(frutas[0])
+print(frutas[1])
+print(frutas[-1])
+
+print(len(frutas))
+print(frutas.count("banana"))
+print(frutas.index("uva"))
+
+for fruta in frutas:
+    print(fruta)
