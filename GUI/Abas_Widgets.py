f309d0e9f8fc351a167401c786551097df9b83c5
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2025-11-12 09:42:25 -0300

    Update Abas_Widgets.py

--- GUI/Abas_Widgets.py
+++ GUI/Abas_Widgets.py
@@ -1,6 +1,16 @@
 import tkinter as tk
 from tkinter import ttk
 
+def select_combo(event):
+    valor = combo_opcoes.get()
+    if valor != "Selecione uma opção":
+        label_estado_civil.config(text = f"Estado Civil: {valor}")
+
+def change_slider(valor): # valor = string
+    print(valor)
+    valor_int = f"{float(valor):.0f}"
+    label_slider.config(text = f"Volume: {valor_int}")
+
 root = tk.Tk()
 root.title("Abas e Widgets")
 root.geometry("450x300")
@@ -20,5 +30,65 @@ notebook.add(aba3, text="Configurações")
 
 # Empacotar o notebook
 notebook.pack(padx=10,pady=10, expand = True, fill = "both")
+
+# Widgets da Aba 1
+label_aba_1 = ttk.Label(aba1,text = "Formulário de Cadastro")
+label_aba_1.pack(padx=20, pady=20)
+entry_nome = ttk.Entry(aba1, width = 40)
+entry_nome.pack(padx = 20, pady = 5)
+btn_salvar = ttk.Button(aba1, text = "Salvar")
+btn_salvar.pack(pady=10)
+
+# Widgets da Aba 2
+label_aba_2 = ttk.Label(aba2,text = "Área de Consulta")
+label_aba_2.pack(padx=20, pady=20)
+btn_buscar = ttk.Button(aba2, text = "Buscar")
+btn_buscar.pack(pady=10)
+
+# Aba 3 - Novos Widgets
+#Variável de controle do checkbox
+var_check = tk.BooleanVar()
+# Estado inicial do checkbox
+var_check.set(False)
+# Widget CheckBox
+check_button = ttk.Checkbutton(aba3, text = "Receber Notificações",
+                               variable=var_check)
+check_button.pack(padx=20, pady=30)
+# Função para buscar o valor da checkbox
+#check_button.getboolean()
+
+# Frame para dropdown e slider
+frame = ttk.LabelFrame(aba3, text="Opções")
+frame.pack(padx=10,pady=10)
+
+lista_estado_civil = [
+    "Selecione uma opção",
+    "Solteiro(a)",
+    "Casado(a)",
+    "Divorciado(a)",
+    "Viúvo(a)"
+]
+# Dropdown (ComboBox)
+combo_opcoes = ttk.Combobox(frame, values= lista_estado_civil, state="readonly")
+combo_opcoes.pack(padx=5,pady=10)
+#Estado inicial
+combo_opcoes.current(0)
+#Label com o estado civil
+label_estado_civil = ttk.Label(frame, text="Estado Civil:")
+label_estado_civil.pack(padx=10, pady=5)
+combo_opcoes.bind("<<ComboboxSelected>>", select_combo)
+
+# Slider(Scale)
+slider = ttk.Scale(
+    frame,
+    from_ = 0,
+    to = 100,
+    orient = "horizontal",
+    command=change_slider
+)
+slider.pack(padx=5,pady=10, fill="x")
+# Label com o valor do slider(scale)
+label_slider = ttk.Label(frame, text = "Volume: 0")
+label_slider.pack(padx=10,pady=5)
 
 root.mainloop()
